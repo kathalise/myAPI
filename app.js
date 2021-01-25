d5e939342aa2760122c7cdd1eca1c92203c9a316
Katharina Wolf <wolfkatharina89@gmail.com>
2021-01-25 13:51:24 +0100

    implement general route

--- app.js
+++ app.js
@@ -27,15 +27,57 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-const article = new Article({
-    _id: "5c18f35cde40ab6cc551cd60",
-    title: "Jack Bauer",
-    content:
-        "Jack Bauer once stepped into quicksand. The quicksand couldn't escape and nearly drowned.",
-    __v: 0,
-});
+// const article = new Article({
+//     _id: "5c18f35cde40ab6cc551cd60",
+//     title: "Jack Bauer",
+//     content:
+//         "Jack Bauer once stepped into quicksand. The quicksand couldn't escape and nearly drowned.",
+//     __v: 0,
+// });
 
 // article.save();
+
+/////// Chaining methods
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        });
+
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Article added to DB!");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Articles have been deleted!");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+////// HTTP Verbs ///////
+// app.get("/articles");
+// app.post("/articles");
+// app.delete("/articles");
+/////////////////////////
 
 app.listen(8080, function () {
     console.log("App is listening on port 8080");
